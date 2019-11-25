24621f2d788781566cd9d8ef78e6f7786239d7a9
Limberth <lvasquezq@ucenfotec.ac.cr>
2019-11-24 20:41:00 -0600

    repository standar methods

--- src/shared/services/mongodb/repository/VerificationKeystorLog.repository.ts
+++ src/shared/services/mongodb/repository/VerificationKeystorLog.repository.ts
@@ -5,25 +5,22 @@ class VerificationKeystorLogRepository extends Repository {
     constructor() {
         super('VerificationKeystorLog');
     }
-    createKeystorLog = async (adminID: string, wareHouseID: string, observations: string, aproved: boolean):
-        Promise<string> => await super.insertOne({ adminID, wareHouseID, observations, aproved });
-
+    create = async (adminID: string, wareHouseID: string, observations: string, aproved: boolean):
+        Promise<any> => {
+            let active = true;
+            await super.insertOne({ adminID, wareHouseID, observations, aproved, active});
+        }
     find = async (where: object): Promise<VerificationKeystorLogModel[]> => await super.find(where);
 
-    getKeystorLog = async (_id: string): Promise<any> => {
-        _id = new ObjectId(_id)
-        await super.find({ _id }).then(arr => arr.shift());
-    }
-
-    async updateOne(set: object, where: object): Promise<any> {
+    async update(set: object, where: object): Promise<any> {
         return await super.updateOne(set, where)
     }
 
-    deleteKeystorLog = async (_id: string):
+    delete = async (_id: string):
         Promise<any> => {
         _id = new ObjectId(_id)
         let active = true;
-        return await this.updateOne({ active }, { _id }).then(result => {
+        return await this.update({ active }, { _id }).then(result => {
             return { rows: result.length, result };
         })
     }
